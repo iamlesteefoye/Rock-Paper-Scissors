0b9435f5b7e31f42a675f8b23c7cf2294b497102
HULKTHEBEAST <ivoryfingers@gmail.com>
2018-07-17 02:08:14 -0400

    july 17

---
+++ assets/javascript/main.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyARwih2D3MGap9v1esjxk6m_6PAYcFF-Fs",
+    authDomain: "rock-paper-scissors-84366.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-84366.firebaseio.com",
+    projectId: "rock-paper-scissors-84366",
+    storageBucket: "rock-paper-scissors-84366.appspot.com",
+    messagingSenderId: "945927405705"
+  };
